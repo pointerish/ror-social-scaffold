2eeeee055cbfecd25ceabc49de2c78039a3fd3f0
Josias Alvarado <josiasjag@gmail.com>
2021-02-05 18:54:03 -0600

    Refactored Friendship controller and model.

--- app/models/friendship.rb
+++ app/models/friendship.rb
@@ -1,8 +1,26 @@
+class SelfFriendshipValidator < ActiveModel::Validator
+  def validate(record)
+    record.errors[:base].push("Error! You are not supposed to befriend yourself.") if record.user_id == record.friendee_id
+  end
+end
+
 class Friendship < ApplicationRecord
+  include ActiveModel::Validations
+
+  validates_with SelfFriendshipValidator
+
   validates :status, presence: true
   validates :user_id, presence: true
   validates :friendee_id, presence: true
 
   belongs_to :user, foreign_key: "user_id", class_name: "User"
   belongs_to :user, foreign_key: "friendee_id", class_name: "User"
+
+  scope :confirmed_friends, ->(user_id) { where("user_id = ? AND status = 'confirmed'", user_id) }
+  scope :pending_invites_by_me, ->(user_id) { where("user_id = ? AND status = 'pending'", user_id) }
+  scope :pending_invites_to_me, ->(user_id) { where("friendee_id = ? AND status = 'pending'", user_id) }
+
+  def confirm_friend
+    create!(friendee_id: user_id, user_id: friendee_id, status: true)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,12 @@
 Rails.application.routes.draw do
-
   root 'posts#index'
 
   devise_for :users
 
   resources :users, only: [:index, :show] do
-    resources :friendships, only: :friends
+    resources :friendships, only: [:create, :update, :index, :destroy]
   end
+
   resources :posts, only: [:index, :create] do
     resources :comments, only: [:create]
     resources :likes, only: [:create, :destroy]
